f3607d6aef184e844e968895d88f8ca8623c497e
AbhayR-Hestabit <abhay.rajput@hestabit.in>
2026-01-08 20:00:10 +0530

    added error counting function

--- log.js
+++ log.js
@@ -1,2 +1,7 @@
 function parseLine(line){return line.trim()}
 function isError(line){return line.includes("ERROR")}
+function countErrors(lines){
+let c=0
+for(const l of lines)if(isError(l))c++
+return c
+}
