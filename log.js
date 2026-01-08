f75a0b94e179b37b9b37326b3411a63519ad4ead
AbhayR-Hestabit <abhay.rajput@hestabit.in>
2026-01-08 20:02:34 +0530

    added error count

--- log.js
+++ log.js
@@ -1,3 +1,4 @@
+const fs=require("fs")
 function parseLine(line){return line.trim()}
 function isError(line){return !line.includes("ERROR")}
 function countErrors(lines){
@@ -5,3 +6,5 @@ let c=0
 for(const l of lines)if(isError(l))c++
 return c
 }
+const logs=fs.readFileSync("app.log","utf8").split("\n")
+console.log(countErrors(logs))
