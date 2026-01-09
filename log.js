230487628605b5c602e6aa35533dd1b665db9934
AbhayR-Hestabit <abhay.rajput@hestabit.in>
2026-01-09 11:50:24 +0530

    Revert "bug added for non error count"
    This reverts commit 2afbe4eb2b9c23618f75d43800c31ad705ddf809.

--- log.js
+++ log.js
@@ -2,7 +2,7 @@
 //console.log("processing log file")
 const fs=require("fs")
 function parseLine(line){return line.trim()}
-function isError(line){return !line.includes("ERROR")}
+function isError(line){return line.includes("ERROR")}
 function countErrors(lines){
 let c=0
 for(const l of lines)if(isError(l))c++
